c8ce5e9b5301bf534100f1e4199621ee1a5b087c
Vyacheslav Frolov <frolov78@gmail.com>
2024-08-15 12:29:02 +0100

    Quick Fix ConcurrentModificationException while iterating Insights listeners

--- maestro-utils/src/main/kotlin/Insight.kt
+++ maestro-utils/src/main/kotlin/Insight.kt
@@ -1,21 +1,35 @@
 package maestro.utils
 
+import org.slf4j.LoggerFactory
+import java.util.concurrent.ConcurrentLinkedQueue
+
 object Insights {
+    private val logger = LoggerFactory.getLogger(Insights::class.java)
 
-    private var insight: Insight = Insight("", Insight.Level.NONE)
-    private val listeners = mutableListOf<(Insight) -> Unit>()
+    private val listeners = ConcurrentLinkedQueue<(Insight) -> Unit>()
 
-    fun report(insight: Insight) {
-        this.insight = insight
-        listeners.forEach { it.invoke(insight) }
+    fun report(insight: Insight?) {
+        if (insight != null) {
+            listeners.forEach { it.invoke(insight) }
+        } else {
+            logger.error("report: The insight object is NULL. The call stack is this:\n" + Thread.currentThread().stackTrace.joinToString("\n"))
+        }
     }
 
-    fun onInsightsUpdated(callback: (Insight) -> Unit) {
-        listeners.add(callback)
+    fun onInsightsUpdated(callback: ((Insight) -> Unit)?) {
+        if (callback != null) {
+            listeners.add(callback)
+        } else {
+            logger.error("onInsightsUpdated: The insight object is NULL. The call stack is this:\n" + Thread.currentThread().stackTrace.joinToString("\n"))
+        }
     }
 
-    fun unregisterListener(callback: (Insight) -> Unit) {
-        listeners.remove(callback)
+    fun unregisterListener(callback: ((Insight) -> Unit)?) {
+        if (callback != null) {
+            listeners.remove(callback)
+        } else {
+            logger.error("unregisterListener: The insight object is NULL. The call stack is this:\n" + Thread.currentThread().stackTrace.joinToString("\n"))
+        }
     }
 }
 
